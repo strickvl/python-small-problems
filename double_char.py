23a2a33936f6e8d615306d9e29b19a47a6d58fec
Sourcery AI <>
2023-11-30 08:54:46 +0000

    'Refactored by Sourcery'

--- double_char.py
+++ double_char.py
@@ -2,7 +2,6 @@ def double_char(s):
 	final_word=""
 	for i in s:
 		doubled_bit=i*2
-		final_word=final_word+doubled_bit
-		return final_word
+		return final_word+doubled_bit
 
 double_char("hope")
--- find_short.py
+++ find_short.py
@@ -1,9 +1,6 @@
 def find_short(s):
     words=s.split()
-    word_lengths=[]
-    for i in words:
-    	word_lengths.append(len(i))
-    l=min(word_lengths)
-    return l
+    word_lengths = [len(i) for i in words]
+    return min(word_lengths)
 
 find_short("hope springs eternal")
